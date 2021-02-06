944da618dc9a4a22ef630b5626ab71cda7968f5c
Matic Bozic <bozicmatic.bm@gmail.com>
2021-02-06 19:43:09 +0100

    Added font for the header, changed navbar to expanding circle

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,29 @@
-var navBlob = document.querySelector('#nav-blob');
+var nav = document.querySelector('nav');
+var toggle = document.querySelector(".toggle");
+var toggle_open_text = 'Menu';
+var toggle_close_text = 'Close';
 
-window.addEventListener('scroll', function() {
-    if (window.pageYOffset > 20) {
-        navBlob.style.top = '0px';
-    } else {
-        navBlob.style.top = (-1 * (navBlob.clientHeight)).toString() + 'px';
-    }
-});
+toggle.addEventListener('click', function() {
+	nav.classList.toggle('open');
+	
+  if (nav.classList.contains('open')) {
+    toggle.innerText = toggle_close_text;
+  } else {
+    toggle.innerText = toggle_open_text;
+  }
+}, false);
+
+setTimeout(function(){
+	nav.classList.toggle('open');	
+}, 800);
+
+
+// window.addEventListener('scroll', function () {
+//     if (window.pageYOffset > 20) {
+//         nav.style.backgroundColor = '#ffbda9';
+//     } else {
+//         nav.style.backgroundColor = 'rgba(0, 0, 0, 0.0)'
+//     }
+// });
+
+
