39da979386f9df6d6cf7d33d65222a1341cace91
Matic Bozic <bozicmatic.bm@gmail.com>
2021-02-08 17:01:20 +0100

    Added full screen navigation with transitions and animations

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,6 @@
-var nav = document.querySelector('nav');
-var toggle = document.querySelector(".toggle");
-var toggle_open_text = 'Menu';
-var toggle_close_text = 'Close';
+var body = document.querySelector('body');
+var menu = document.querySelector('.menu-icon');
 
-toggle.addEventListener('click', function() {
-	nav.classList.toggle('open');
-	
-  if (nav.classList.contains('open')) {
-    toggle.innerText = toggle_close_text;
-  } else {
-    toggle.innerText = toggle_open_text;
-  }
-}, false);
-
-setTimeout(function(){
-	nav.classList.toggle('open');	
-}, 800);
-
-
-// window.addEventListener('scroll', function () {
-//     if (window.pageYOffset > 20) {
-//         nav.style.backgroundColor = '#ffbda9';
-//     } else {
-//         nav.style.backgroundColor = 'rgba(0, 0, 0, 0.0)'
-//     }
-// });
-
-
+menu.addEventListener('click', function() {
+  body.classList.toggle('nav-active');
+});
