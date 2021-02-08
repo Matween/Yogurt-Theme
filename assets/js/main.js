d66c6f8a7d2198c3100292f33d5ab9b1c372279d
Matic Bozic <bozicmatic.bm@gmail.com>
2021-02-08 17:15:30 +0100

    Fix the issue where nav is always on top, even when not visible, items are now clickable

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,12 @@
 var body = document.querySelector('body');
 var menu = document.querySelector('.menu-icon');
+var nav = document.querySelector('nav');
 
 menu.addEventListener('click', function() {
   body.classList.toggle('nav-active');
+  if (body.classList.contains('nav-active')) {
+    nav.style.zIndex = '1'
+  } else {
+    nav.style.zIndex = '-1';
+  }
 });
